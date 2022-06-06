629e5a1299f8cb58f8318b04720706670ea14613
Flemis <coderflemis@gmail.com>
2022-06-06 05:35:09 -0300

    adding first version of the sh script for launching dmenu

--- src/dmenu.rs
+++ src/dmenu.rs
@@ -5,12 +5,14 @@ use std::io::{BufReader, BufWriter};
 
 use crate::data::Repos;
 
+//TODO: Add permission to execute to repo_list file
 pub fn create_dmenu_repo_list(repos: &Vec<Repos>) {
     let config_path = dirs_next::config_dir()
         .unwrap()
-        .join("quickGHMenu/repos_list");
+        .join("quickGHMenu/repo_list");
 
-    let mut repo_list: String = "#Repo list\ndeclare -A github_repo_list\n\n".to_owned();
+    let mut repo_list: String =
+        "#!/bin/bash\n#Repo list\ndeclare -A github_repo_list\n\n".to_owned();
     for repo in repos {
         let line: String =
             format!("github_repo_list[{}]=\"{}\"\n", repo.name, repo.html_url).to_owned();
